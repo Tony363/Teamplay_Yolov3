5b57ad7de5c94fc23b31b269efb05d7da33b3794
tony363 <pysolver33@gmail.com>
2020-06-14 17:32:47 +0800

    dependency hell with opencv. try anaconda

--- Super_Resolution/SuperResolution.py
+++ Super_Resolution/SuperResolution.py
@@ -1,15 +1,22 @@
 import cv2
 from cv2 import dnn_superres
-# Create an SR object - only function that differs from c++ code
+
+# Create an SR object
 sr = dnn_superres.DnnSuperResImpl_create()
+
 # Read image
-image = cv2.imread('./image.png')
+image = cv2.imread('./input1.png')
+
 # Read the desired model
-path = "EDSR_x4.pb"
+path = "EDSR_x3.pb"
 sr.readModel(path)
+
 # Set the desired model and scale to get correct pre- and post-processing
-sr.setModel("edsr", 4)
+sr.setModel("edsr", 3)
+
 # Upscale the image
 result = sr.upsample(image)
+
 # Save the image
 cv2.imwrite("./upscaled.png", result)
+
