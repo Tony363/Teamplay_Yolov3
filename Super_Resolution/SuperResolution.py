2926e991f758ab0d8d97e0210bfb4ec1b567b3d3
tony363 <pysolver33@gmail.com>
2020-06-16 17:43:06 +0800

    Super resolution sample from opencv

--- Super_Resolution/SuperResolution.py
+++ Super_Resolution/SuperResolution.py
@@ -1,23 +1,16 @@
 import cv2
 from cv2 import dnn_superres
-
-# Create an SR object
+# Create an SR object - only function that differs from c++ code
 sr = dnn_superres.DnnSuperResImpl_create()
-
 # Read image
-image = cv2.imread('./input.png')
-
+image = cv2.imread('./image.png')
 # Read the desired model
-path = "EDSR_x3.pb"
+path = "EDSR_x4.pb"
 sr.readModel(path)
-
 # Set the desired model and scale to get correct pre- and post-processing
-instance = sr.setModel("edsr", 3)
-
+sr.setModel("edsr", 4)
 # Upscale the image
 result = sr.upsample(image)
-
 # Save the image
 cv2.imwrite("./upscaled.png", result)
-
 
